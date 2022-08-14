3015d2d7bbf2dc2227c9adb4017d6ec89dcb7119
JD-edu <96219601+JD-edu@users.noreply.github.com>
2022-08-14 13:24:26 +0900

    Create can_selector.ino

---
+++ dimigo/can_selector.ino
@@ -0,0 +1,48 @@
+#include<Servo.h>
+
+Servo sv;
+#define 2 IR
+#define 4 MOTOR_A
+#define 5 MOTOR_B
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(IR,INPUT) // IR sensor  
+  sv.attach(3);
+}
+
+void motor_stop(){
+  digitaWrite(MOTOR_A, LOW);
+  digitaWrite(MOTOR_B, LOW);
+}
+
+void motor_on(){
+  digitaWrite(MOTOR_A, HIGH);
+  digitaWrite(MOTOR_B, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(IR) == 1) // can contected 
+  {
+     motor_stop()
+     Srial.write("s\r\n");
+     
+  }else{
+    while(Serial.Available() > 0){
+      string cmd = Serial.readStringUntil('\n');
+      if (cmd == "can"){
+        delay(3000);
+        sv.write(180); // can
+        delay(3000);
+        motor_on()
+      }else if(cmd == "pet"){
+        delay(1000);
+        sv.write(0);
+        delay(3000);
+        motor_on()
+      }
+    }
+  }
+}
